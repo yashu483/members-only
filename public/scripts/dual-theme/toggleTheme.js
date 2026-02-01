d07d06e6357700faedfa4a383613fbb2093e7605
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2026-02-01 12:33:39 +0530

    feat: make theme toggle button work properly

--- public/scripts/dual-theme/toggleTheme.js
+++ public/scripts/dual-theme/toggleTheme.js
@@ -1,4 +1,5 @@
 const toggle = document.querySelector("#theme-toggle");
+const toggleBig = document.querySelector("#theme-toggle-big");
 
 toggle.addEventListener("click", (e) => {
   let theme = localStorage.getItem("theme");
@@ -10,6 +11,18 @@ toggle.addEventListener("click", (e) => {
     toggle.textContent = "Dark Theme";
   }
 });
+
+toggleBig.addEventListener("click", (e) => {
+  let theme = localStorage.getItem("theme");
+  if (theme === "light") {
+    setDarkTheme();
+    toggle.textContent = "Light Theme";
+  } else {
+    setLightTheme();
+    toggle.textContent = "Dark Theme";
+  }
+});
+
 function setDarkTheme() {
   localStorage.setItem("theme", "dark");
   document.documentElement.setAttribute("data-theme", "dark");
